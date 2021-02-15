320261d87b4b97f570f5ab82c6b7f0f50177f09a
Miguel R. Ávila <miguelravila@pm.me>
2021-02-15 15:28:49 -0600

    Initial run!

---
+++ src/main/java/com/lintori/popcornsapi/PopcornsapiApplication.java
@@ -0,0 +1,13 @@
+package com.lintori.popcornsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PopcornsapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PopcornsapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lintori/popcornsapi/PopcornsapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lintori.popcornsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PopcornsapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

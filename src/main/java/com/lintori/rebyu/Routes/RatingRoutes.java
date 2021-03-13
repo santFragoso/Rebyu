f9b5c404aef92908805ab09046fc538a90b6c78a
Jorge Reyes <reyestrevjorge@gmail.com>
2021-03-12 18:17:30 -0600

    feat: Rating CRUD implemented
    Implemented the basic Rating resource CRUD routes :rocket:. closes #4

---
+++ src/main/java/com/lintori/rebyu/Routes/RatingRoutes.java
@@ -0,0 +1,46 @@
+package com.lintori.rebyu.Routes;
+
+import com.lintori.rebyu.Entities.Rating;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+
+@RestController
+@RequestMapping("/api/ratings")
+public class RatingRoutes {
+
+    @GetMapping()
+    public ArrayList<Rating> getAllRatings() {
+        ArrayList<Rating> allRatings = new ArrayList<>();
+        allRatings.add(new Rating());
+        return allRatings;
+    }
+
+    @GetMapping("/{id}")
+    public Rating getRating(
+            @PathVariable(value="id") int requestID
+    ) {
+        return new Rating();
+    }
+
+    @PostMapping()
+    public void postRating(
+            @RequestBody Rating requestRating
+    ) {
+
+    }
+
+    @PutMapping("/{id}")
+    public void putRating(
+            @PathVariable(value="id") int requestID
+    ) {
+
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteRating(
+            @PathVariable(value="id")  int requestID
+    ) {
+
+    }
+}

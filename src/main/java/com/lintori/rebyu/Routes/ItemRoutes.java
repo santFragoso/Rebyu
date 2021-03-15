b7b90fceb148c3bc0510235757f8b1ae9428836a
Jorge Reyes <reyestrevjorge@gmail.com>
2021-03-15 16:06:49 -0600

    :recycle: refactor: Movie entity name
    Refactor the Movie entity to the Item entity.

--- src/main/java/com/lintori/rebyu/Routes/ItemRoutes.java
+++ src/main/java/com/lintori/rebyu/Routes/ItemRoutes.java
@@ -1,6 +1,6 @@
 package com.lintori.rebyu.Routes;
 
-import com.lintori.rebyu.Entities.Movie;
+import com.lintori.rebyu.Entities.Item;
 import com.lintori.rebyu.Entities.User;
 import com.lintori.rebyu.Generic.Date;
 import org.springframework.web.bind.annotation.*;
@@ -8,13 +8,13 @@ import org.springframework.web.bind.annotation.*;
 import java.util.ArrayList;
 
 @RestController
-@RequestMapping("/api/movies")
-public class MovieRoutes {
+@RequestMapping("/api/items")
+public class ItemRoutes {
 
     @GetMapping()
-    public ArrayList<Movie> getAllMovies() {
-        ArrayList<Movie> allMovies = new ArrayList<>();
-        allMovies.add(new Movie(
+    public ArrayList<Item> getAllItems() {
+        ArrayList<Item> allItems = new ArrayList<>();
+        allItems.add(new Item(
                 1,
                 "title",
                 new User(
@@ -35,14 +35,14 @@ public class MovieRoutes {
                         15
                 )
         ));
-        return allMovies;
+        return allItems;
     }
 
     @GetMapping("/{id}")
-    public Movie getMovie(
+    public Item getItem(
             @PathVariable(value="id") int requestID
     ) {
-        return new Movie(
+        return new Item(
                 1,
                 "title",
                 new User(
@@ -66,21 +66,21 @@ public class MovieRoutes {
     }
 
     @PostMapping()
-    public void postMovie(
-            @RequestBody Movie requestMovie
+    public void postItem(
+            @RequestBody Item requestItem
     ) {
 
     }
 
     @PutMapping("/{id}")
-    public void putMovie(
+    public void putItem(
             @PathVariable(value="id") int requestID
     ) {
 
     }
 
     @DeleteMapping("/{id}")
-    public void deleteMovie(
+    public void deleteItem(
             @PathVariable(value="id") int requestID
     ) {
 
